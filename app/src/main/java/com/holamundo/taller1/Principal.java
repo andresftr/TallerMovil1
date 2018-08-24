c92f3f4cbadabdb312cd53ce75b5ba60a4c7bd5f
Andres Tarra <afelipe0113@gmail.com>
2018-08-23 21:21:09 -0500

    Primera parte interfaz taller 1

---
+++ app/src/main/java/com/holamundo/taller1/Principal.java
@@ -0,0 +1,13 @@
+package com.holamundo.taller1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Principal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
